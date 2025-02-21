5f58edfd77a99bafa17b131629f14c7c6c824bd8
omgirimonte <owen.girimonte@gmail.com>
2025-02-21 18:43:48 +0000

    update 2/21/2025

--- BSTProject/src/BST.java
+++ BSTProject/src/BST.java
@@ -1,5 +1,4 @@
 import java.lang.Comparable;
-
 public class BST implements BSTInterface
 {
     private TreeNode root;
@@ -8,43 +7,110 @@ public class BST implements BSTInterface
         root = null;
     }
 
-    public void add(Comapareable newVal){
+    public void add(Comparable newVal){
         if (root==null){
-            root = newVal;
-            return false;
+            root = new TreeNode(newVal);
         }
         else{
             if (newVal.compareTo(root)<=0){
-                if (root.leftChild()==null){
-                    root.setLeft(newVal);
+                if (root.getLeft()==null){
+                    root.setLeft(new TreeNode(newVal));
                 }
                 else{
-                    addHelper(newVal,root.leftChild());
+                    addHelper(newVal,root.getLeft());
                 }
+            }
             else{
-                addHelper(newVal,root.rightChild());
+                if(root.getRight()==null){
+                    root.setRight(new TreeNode(val));
+                }
+                else{
+                    addHelper(newVal,root.getRight());
+                }
             }
-            }
+            
         }
     }// end add
 
-    private void addHelper(Comapareable val, TreeNode subroot){
+    private void addHelper(Comparable val, TreeNode subroot){
         if (val.compareTo(subroot)<=0){
-            if (subroot.leftChild()==null){
-                subroot.setLeft(val);
+            if (subroot.getLeft()==null){
+                subroot.setLeft(new TreeNode(val));
             }
             else{
-                addHelper(val,subroot.leftChild());
+                addHelper(val,subroot.getLeft());
             }
+        }
+            
         else{
-            if (subroot.rightChild()==null){
-                subroot.setRight(val);
+            if (subroot.getRight()==null){
+                subroot.setRight(new TreeNode(val));
             }
             else{
-                addHelper(val,subroot.rightChild());
+                addHelper(val,subroot.getRight());
             }
         }
+        }// end addhelper
+    
+
+
+
+    public void printInOrder(){
+        if (root!=null){
+            printInHelper(root.getLeft());
+            System.out.println(root.getValue());
+            printInHelper(root.getRight());
         }
-    }// end addhelper
+
+    }
+
+    private void printInHelper(TreeNode subroot){
+        if (subroot!=null){
+            printInHelper(subroot.getLeft());
+            System.out.println(subroot.getValue());
+            printInHelper(subroot.getRight());
+        }
+
+    }
+
+
+
+
+
+    public void printPostOrder(){
+        if (root!=null){
+            printPostHelper(root.getLeft());
+            printPostHelper(root.getRight());
+            System.out.println(root.getValue());
+        }
+    }
+
+    private void printPostHelper(TreeNode subroot){
+        if (subroot!=null){
+            printPostHelper(subroot.getLeft());
+            printPostHelper(subroot.getRight());
+            System.out.println(subroot.getValue());
+        }
+    }
+
+
+
+    public void printPreOrder(){
+        if (root!=null){
+            System.out.println(root.getValue());
+            printPreHelper(root.getLeft());
+            printPreHelper(root.getRight());
+        }
+    }
+
+
+
+    private void printPreHelper(TreeNode subroot){
+        if (subroot!=null){
+            System.out.println(subroot.getValue());
+            printPreHelper(subroot.getLeft());
+            printPreHelper(subroot.getRight());
+        }
+    }
         
 }
--- BSTProject/src/BSTTester.java
+++ BSTProject/src/BSTTester.java
@@ -1,14 +1,32 @@
 /*
- * TODO: Name
- * TODO: Date
- * TODO: Class Period
- * TODO: Program Description
+ * TODO: Owen Girimonte 
+ * TODO: Date 2/21/2025
+ * TODO: Class Period 7
+ * TODO: Program Description 
  */
 
  import java.lang.Comparable;
 
  public class BSTTester {
    public static void main(String[] args) {
-   
+   BST tree = new BST();
+   tree.add("G");
+   tree.add("A");
+   tree.add("I");
+   tree.add("T");
+   tree.add("H");
+   tree.add("E");
+   tree.add("E");
+   tree.add("R");
+   tree.add("S");
+   tree.add("B");
+   tree.add("U");
+   tree.add("R");
+   tree.add("G");
+
+   tree.printInOrder();
+   tree.printPostOrder();
+   tree.printPreOrder();
 	
  }
+}
